079adb31ff2a3a2d0b69dcd72c4f899aa772e30c
Stephanie <55038687+stephypy@users.noreply.github.com>
2019-12-02 22:28:06 -0700

    Update EditDistance.py

--- EditDistance.py
+++ EditDistance.py
@@ -1,28 +1,21 @@
-def edit_distance(s1, s2):
-    array = [[0 for i in range(len(s2))] for j in range(len(s1))]
+class ED:
+    def edit_distance(self, s1, s2):
+        array = [[0 for i in range(len(s2) + 1)] for j in range(len(s1) + 1)]
 
-    for i in range(len(array)):
-        for j in range(len(array[i])):
-            if i == 0 and j == 0:
-                if s1[i] != s2[j]:
-                    array[i][j] += 1
-            elif i == 0:
-                if s1[i] != s2[j]:
-                    array[i][j] = array[i][j - 1] + 1
-            elif j == 0:
-                if s1[i] != s2[j]:
-                    array[i][j] = array[i - 1][j] + 1
-            else:
-                if s1[i] == s2[j]:
-                    array[i][j] = array[i - 1][j - 1]
+        for i in range(len(array)):
+            for j in range(len(array[i])):
+                if i == 0 and j == 0:
+                    continue
+                elif i == 0:
+                    if '' != s2[j - 1]:
+                        array[i][j] = array[i][j - 1] + 1
+                elif j == 0:
+                    if s1[i - 1] != '':
+                        array[i][j] = array[i - 1][j] + 1
                 else:
-                    array[i][j] = min(array[i - 1][j - 1], array[i - 1][j], array[i][j - 1]) + 1
-    return array[-1][-1]
+                    if s1[i - 1] == s2[j - 1]:
+                        array[i][j] = array[i - 1][j - 1]
+                    else:
+                        array[i][j] = min(array[i - 1][j - 1], array[i - 1][j], array[i][j - 1]) + 1
+        return array[-1][-1]
 
-
-def main():
-    print(edit_distance('miner', 'money'))  # ans = 2
-    print(edit_distance('greeks', 'geeks'))  # ans = 1
-
-
-main()
